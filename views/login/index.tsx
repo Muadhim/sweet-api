0dc8bafda66bd228e5aab8f9c0fc5f5d66f53e94
muadhim <muazim2911@gmail.com>
2024-08-05 14:02:52 +0700

    [feat] add page dashboard, sign-in, sign-out, login

--- views/login/index.tsx
+++ views/login/index.tsx
@@ -1,43 +1,43 @@
-"use client";
-import React, { useState } from "react";
+import React, { FormEvent } from "react";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { signIn } from "next-auth/react";
-import { useRouter, useSearchParams } from "next/navigation";
+import { signIn } from "@/auth";
+import { Separator } from "@radix-ui/react-dropdown-menu";
 import { SignInResponse } from "next-auth/react";
-import { Route } from "next";
+import { redirect } from "next/navigation";
+
 const LoginView = () => {
-	const router = useRouter();
-	const searchParams = useSearchParams();
-
-	const [username, setUsername] = useState("");
-	const [password, setPassword] = useState("");
-
-	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-		const callbackUrl = searchParams.get("callbackUrl");
-		e.preventDefault();
-		signIn("credentials", {
+	const onSignIn = async () => {
+		"use server";
+		signIn("Credentials", {
 			redirect: false,
-			username,
-			password,
+			redirectTo: '/dashboard'
 		}).then((res: SignInResponse | undefined) => {
+			console.log("resuslta:", res);
 			if (!res) {
 				alert("No response!");
 				return;
 			}
-
 			if (!res.ok) alert("Something went wrong!");
 			else if (res.error) {
 				console.log(res.error);
-
-				if (res.error == "CallbackRouteError")
-					alert("Could not login! Please check your credentials.");
-				else alert(`Internal Server Error: ${res.error}`);
+				alert(
+					res.error === "CallbackRouteError"
+						? "Could not login! Please check your credentials."
+						: `Internal Server Error: ${res.error}`
+				);
 			} else {
-				if (callbackUrl) router.push(callbackUrl as Route);
-				else router.push("/");
+				// redirect(callbackUrl);
+				// else router.push("/");
 			}
+		});
+	};
+
+	const onGithubSignIn = async () => {
+		"use server";
+		await signIn("github", {redirectTo: "/dashboard"}).then((res) => {
+			console.log("res", res);
 		});
 	};
 
@@ -45,29 +45,38 @@ const LoginView = () => {
 		<div className="-mx-3 mt-32">
 			<Card className="glass w-full m-3 h-[500px] max-w-[400px] mx-auto px-8 py-20 flex justify-center flex-col gap-4">
 				<h1 className="text-center text-xl font-bold">SIGN IN</h1>
-				<form
-					onSubmit={handleSubmit}
-					className="flex justify-center flex-col gap-4"
-				>
-					<Input
-						placeholder="Email"
-						type="email"
-						name="email"
-						required
-						onChange={(e) => setUsername(e.target.value)}
-					/>
+				<form action={onSignIn} className="flex justify-center flex-col gap-4">
+					<Input placeholder="Email" type="email" name="email" required />
 					<Input
 						placeholder="Password"
 						type="password"
 						name="password"
 						required
-						onChange={(e) => setPassword(e.target.value)}
 					/>
 					<br />
-					<div className="text-sm flex justify-between font-light">
-						<a href="/forgot">Forgot password?</a>
-					</div>
+					<a
+						className="text-sm flex justify-between font-light"
+						href="/forgot-password"
+					>
+						Forgot password?
+					</a>
 					<Button type="submit">Sign In</Button>
+				</form>
+
+				<div className="flex justify-center items-center w-full">
+					<Separator className="w-1/2 " />
+					<p>OR</p>
+					<Separator className="w-1/2" />
+				</div>
+
+				<form className="w-full" action={onGithubSignIn}>
+					<Button
+						variant="outline"
+						type="submit"
+						className="w-full bg-secondary"
+					>
+						Signin with GitHub
+					</Button>
 				</form>
 			</Card>
 		</div>
