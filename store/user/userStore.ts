34d5bbd1dfe03521b6db891119c9ce7e911d7324
muadhim <muazim2911@gmail.com>
2024-08-18 22:16:39 +0700

    [feat] create store for auth

--- store/user/userStore.ts
+++ store/user/userStore.ts
@@ -1,9 +1,16 @@
 import { create } from "zustand";
+import { User } from "@/interfaces/user";
 
-const useUserStore = create((set) => ({
-	user: {} as any,
-	setUser: (user: any) => set(user),
-	clearUser: () => set({ user: null }),
+interface UserState {
+  user: User | null;
+  setUser: (user: any) => void;
+  clearUser: () => void;
+}
+
+const useUserStore = create<UserState>((set) => ({
+  user: null,
+  setUser: (user: any) => set(user),
+  clearUser: () => set({ user: null }),
 }));
 
 export default useUserStore;
