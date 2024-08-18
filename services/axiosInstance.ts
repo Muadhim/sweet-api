159cd8cd460daed66eacdd9ce8fc6791836110ea
muadhim <muazim2911@gmail.com>
2024-08-18 22:16:04 +0700

    [feat] add header content type

--- services/axiosInstance.ts
+++ services/axiosInstance.ts
@@ -1,5 +1,9 @@
 import axios, { AxiosInstance } from "axios";
 
 export const api: AxiosInstance = axios.create({
-	baseURL: process.env.NEXT_PUBLIC_API_URL_ORIGIN,
+  baseURL: process.env.NEXT_PUBLIC_API_URL_ORIGIN,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  // withCredentials: true,
 });
