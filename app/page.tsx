c343fce4cc5d9d65518dd483b9d40aaa39c6fd0d
muadhim <muazim2911@gmail.com>
2024-08-18 22:13:42 +0700

    [feat] make page route to client side

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
+"use client";
 import HomeView from "@/views/home";
 import * as React from "react";
 
 export default function Home() {
-	return <HomeView />;
+  return <HomeView />;
 }
--- views/home/index.tsx
+++ views/home/index.tsx
@@ -1,34 +1,27 @@
-import { redirect } from "next/navigation";
+import Link from "next/link";
 import React from "react";
 
 const HomeView = () => {
-	const handleRegisterClick = async () => {
-		"use server";
-		redirect("/register");
-	};
-
-	return (
-		<div className="flex flex-col text-center justify-center mt-32 gap-y-4">
-			<h1 className="text-2xl font-bold text-primary">Welcome to Sweet API!</h1>
-			<div className="w-[600px] mx-auto text-center">
-				<h2 className="text-secondary-foreground">
-					Create and manage your API documentation effortlessly. Best of all,
-					Sweet API is completely free to use. Start today and enjoy all our
-					features at no cost!
-				</h2>
-				<div className="flex gap-1 justify-center mt-2">
-					<p className="text-secondary-foreground">
-						What are you waiting for?{" "}
-					</p>
-					<form action={handleRegisterClick}>
-						<button type="submit" className="text-primary font-bold">
-							Register Now!
-						</button>
-					</form>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex flex-col text-center justify-center mt-32 gap-y-4">
+      <h1 className="text-2xl font-bold text-primary">Welcome to Sweet API!</h1>
+      <div className="w-[600px] mx-auto text-center">
+        <h2 className="text-secondary-foreground">
+          Create and manage your API documentation effortlessly. Best of all,
+          Sweet API is completely free to use. Start today and enjoy all our
+          features at no cost!
+        </h2>
+        <div className="flex gap-1 justify-center mt-2">
+          <p className="text-secondary-foreground">
+            What are you waiting for?{" "}
+          </p>
+          <Link href="/register" className="text-primary font-bold">
+            Register Now!
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomeView;
