ea832a57bbcc21494d7030e0064725daec26b0d1
muadhim <muazim2911@gmail.com>
2024-08-24 21:05:51 +0700

    [feat] add axios interceptors

--- services/axiosInstance.ts
+++ services/axiosInstance.ts
@@ -5,6 +5,26 @@ export const api: AxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL_ORIGIN,
   headers: {
     "Content-Type": "application/json",
-    Authorization: `Bearer ${getUserCookie()?.access_token}`,
   },
 });
+
+api.interceptors.request.use(
+  (config) => {
+    Object.assign(config.headers, {
+      Authorization: `Bearer ${getUserCookie()?.access_token}`,
+    });
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.status === 401) {
+      window.location.href = "/sign-in";
+    }
+    return Promise.reject(error);
+  }
+);
