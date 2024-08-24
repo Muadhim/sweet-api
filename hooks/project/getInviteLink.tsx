edbc6ff5fd095402d5a1231457e7d4452560efb2
muadhim <muazim2911@gmail.com>
2024-08-24 22:58:30 +0700

    [feat] add get invite link hooks

---
+++ hooks/project/getInviteLink.tsx
@@ -0,0 +1,18 @@
+import { useGetInviteLink } from "@/services/project";
+import { useState } from "react";
+
+const useGetInviteLinkHooks = () => {
+  const [pId, setPid] = useState<number>(0);
+  const { data: token, isLoading } = useGetInviteLink(pId);
+  return {
+    data: {
+      token,
+      isLoading,
+    },
+    method: {
+      setPid,
+    },
+  };
+};
+
+export default useGetInviteLinkHooks;
