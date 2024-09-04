c797aabe654e3d66a1ae33dcccfedde3f7d9fa02
muadhim <muazim2911@gmail.com>
2024-09-04 20:40:50 +0700

    [feat] create get group hooks

---
+++ hooks/group/getGroups.ts
@@ -0,0 +1,18 @@
+import { useGetGroups } from "@/services/group";
+import useGroupStore from "@/store/group/indext";
+import { useEffect } from "react";
+
+const useGetGroupsHooks = () => {
+  const { data, isLoading } = useGetGroups();
+  const setGroups = useGroupStore((state) => state.setGroups);
+
+  useEffect(() => {
+    if (data && data.data) setGroups(data.data);
+    else setGroups([]);
+  }, [data]);
+
+  return {
+    data: { isLoading },
+  };
+};
+export default useGetGroupsHooks;
