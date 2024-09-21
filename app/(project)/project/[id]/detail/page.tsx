addf23181db2ed420304573523f0862716a550fc
muadhim <muazim2911@gmail.com>
2024-09-21 09:49:37 +0700

    [feat] create detail project page

--- app/(project)/project/[id]/detail/page.tsx
+++ app/(project)/project/[id]/detail/page.tsx
@@ -1,7 +1,12 @@
+import ProjectDetailView from "@/views/project_detail";
 import React from "react";
 
 const ProejctDetail = ({ params }: { params: { id: string } }) => {
-  return <div>ProejctDetail: {params.id}</div>;
+  return (
+    <div className="flex flex-col">
+      <ProjectDetailView projectId={Number(params.id)} />
+    </div>
+  );
 };
 
 export default ProejctDetail;
