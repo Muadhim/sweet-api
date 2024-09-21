cd2f9fa916a8386a404e454574b9aeed3c3aec19
muadhim <muazim2911@gmail.com>
2024-09-21 09:51:33 +0700

    [feat] create store for project tree

--- store/project/index.ts
+++ store/project/index.ts
@@ -1,20 +1,24 @@
 import { IProject } from "@/interfaces/Project";
+import { IProjectTreeResponse } from "@/interfaces/ProjectTree";
 import { create } from "zustand";
 
 type State = {
   projects: IProject[];
   project: IProject;
+  projectTree: IProjectTreeResponse;
 };
 
 type Actions = {
   setProjects: (p: IProject[]) => void;
   setProject: (p: IProject) => void;
+  setProjectTree: (pt: IProjectTreeResponse) => void;
   reset: () => void;
 };
 
 const initialState: State = {
   projects: [],
   project: {} as IProject,
+  projectTree: {} as IProjectTreeResponse,
 };
 
 const useProjectStore = create<State & Actions>()((set, get) => ({
@@ -24,6 +28,9 @@ const useProjectStore = create<State & Actions>()((set, get) => ({
   },
   setProject: (p: IProject) => {
     set({ project: p });
+  },
+  setProjectTree: (pt: IProjectTreeResponse) => {
+    set({ projectTree: pt });
   },
   reset: () => {
     set(initialState);
